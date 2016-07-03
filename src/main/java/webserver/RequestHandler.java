c348720575eeb6153e2511c976231788120fe8cd
Minho Kim <eoriented@gmail.com>
2016-07-03 17:03:02 +0900

    [JAVA-CAMP-1] First requirement for web application server

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,16 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +28,26 @@ public class RequestHandler extends Thread {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			
+			Map<String, String> header = new HashMap<>();
+			
+			String line = br.readLine();
+			if (line == null) {
+				return;
+			}
+			String url = line.split(" ")[1];
+			
+			while (!"".equals(line)) {
+				String[] headerTokens = line.split(": ");
+				if (headerTokens.length == 2)
+					header.put(headerTokens[0], headerTokens[1]);
+				
+				line = br.readLine();
+			}
+			
+			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
